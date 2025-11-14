08ded62232ac2737287cadb77bfe4900afa63107
Göktuğ Özfen <goktugozfen@gmail.com>
2025-11-14 19:38:05 +0300

    .

---
+++ TraversingDOM/script.js
@@ -0,0 +1,37 @@
+// // Get child elements
+// let output;
+
+// const parent = document.querySelector(".parent");
+
+// output = parent.children;
+
+// output = parent.children[0].innerText;
+// output = parent.children[1].className;
+// output = parent.children[1].nodeName;
+
+// parent.children[1].innerText = "Child Two";
+// parent.children[1].style.color = "purple";
+
+// parent.firstElementChild.innerText = "Child One";
+// parent.lastElementChild.innerText = "Child Three";
+
+// // Get parent element from child
+
+// const child = document.querySelector(".child");
+
+// output = child.parentElement;
+
+// child.parentElement.style.border = "10px solid purple";
+// child.parentElement.style.padding = "10px";
+// child.parentElement.style.margin = "10px";
+
+// // Sibling elements
+
+// const secondItem = document.querySelector(" .child:nth-child(2)");
+
+// output = secondItem;
+// output = secondItem.nextElementSibling;
+
+// secondItem.nextElementSibling.style.color = "green";
+// secondItem.previousElementSibling.style.color = "orange"
+// console.log(output);
--- shoppling-list/singleElement.js
+++ shoppling-list/singleElement.js
@@ -1,41 +1,59 @@
-// DOM selectors single elements
+// // DOM selectors single elements
 
-//document.getElementById()
+// //document.getElementById()
 
-console.log(document.getElementById("app-title"));
-console.log(document.getElementById("app-title").id);
-console.log(document.getElementById("app-title").className);
+// console.log(document.getElementById("app-title"));
+// console.log(document.getElementById("app-title").id);
+// console.log(document.getElementById("app-title").className);
 
-console.log(document.getElementById("app-title").getAttribute("id"));
+// console.log(document.getElementById("app-title").getAttribute("id"));
 
-// Set attributes
+// // Set attributes
 
-document.getElementById("app-title").title = "Shopping List";
+// document.getElementById("app-title").title = "Shopping List";
 
-document.getElementById("app-title").setAttribute("class", "title");
+// document.getElementById("app-title").setAttribute("class", "title");
 
-const title = document.getElementById("app-title");
+// const title = document.getElementById("app-title");
 
-// console.log(title);
+// // console.log(title);
 
-// Get/change content
-console.log(title.textContent);
-title.textContent = "hello sluts";
-title.innerText = "Hello Fuck Face";
-//text we html yapısını değiştirmek için
+// // Get/change content
+// console.log(title.textContent);
+// title.textContent = "hello sluts";
+// title.innerText = "Hello Fuck Face";
+// //text we html yapısını değiştirmek için
 
-title.innerHTML = "<strong>Shopping List</strong>";
+// title.innerHTML = "<strong>Shopping List</strong>";
 
-// Change style
+// // Change style
 
-title.style.color = "red";
-title.style.backgroundColor = "black";
-title.style.padding = "10px";
-title.style.borderRadius = "10px";
+// title.style.color = "red";
+// title.style.backgroundColor = "black";
+// title.style.padding = "10px";
+// title.style.borderRadius = "10px";
 
-// document.querySelector()
+// // document.querySelector()
 
-console.log(document.querySelector("h1"));
-console.log(document.querySelector("#app-title"));
-console.log(document.querySelector(".container"));
+// console.log(document.querySelector("h1"));
+// console.log(document.querySelector("#app-title"));
+// console.log(document.querySelector(".container"));
+// console.log(document.querySelector("input[type=Text]"));
 
+// console.log(document.querySelector("li:nth-child(2)").innerText);
+
+// const secondItem = console.log(
+//   document.querySelector("li:nth-child(2)").innerText
+// );
+
+// const secondItem1 = document.querySelector("li:nth-child(2)");
+
+// secondItem1.innerText = "Dalga Juice";
+// secondItem1.style.color = "violet";
+
+// // Use these methods on other elements
+
+// const list = document.querySelector("ul");
+// console.log(list);
+// const firstItem = document.querySelector("li");
+// firstItem.style.color="blue"
