d14cf6996264a14a46126e3067c5c13c149be057
Göktuğ Özfen <goktugozfen@gmail.com>
2025-11-15 18:14:57 +0300

    .

---
+++ Create&AppendElements/script.js
@@ -0,0 +1,16 @@
+// Create & Append Elements
+
+const div = document.createElement("div");
+div.className = "my-element";
+div.id = "my-element";
+div.setAttribute("title", "My fuckn Element");
+
+// div.innerText ="Hello World"
+
+const text = document.createTextNode("Hello World");
+div.appendChild(text)
+
+// console.log(div);
+document.body.appendChild(div)
+
+document.querySelector("ul").appendChild(div)
